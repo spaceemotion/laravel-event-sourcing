4ab140a3f4acfd96559dec09ee0ec146fc178f14
André Gräßel <hello@spaceemotion.net>
2019-11-17 16:10:31 +0100

    Add built-in AggregateId that uses Str::uid()

---
+++ src/Ids/Uuid.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spaceemotion\LaravelEventSourcing\Ids;
+
+use Illuminate\Support\Str;
+use Spaceemotion\LaravelEventSourcing\AggregateId;
+
+final class Uuid implements AggregateId
+{
+    /** @var string */
+    private $id;
+
+    private function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+    public static function fromString(string $id): AggregateId
+    {
+        return new static($id);
+    }
+
+    public function __toString(): string
+    {
+        return $this->id;
+    }
+
+    public static function next(): AggregateId
+    {
+        return self::fromString(Str::uuid()->toString());
+    }
+}
