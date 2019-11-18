420bfbb8030546d69caabd7c49c5ac0f7cfd6c99
André Gräßel <hello@spaceemotion.net>
2019-11-18 23:04:15 +0100

    Add basic AggregateRoot tests

---
+++ tests/AggregateRootTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spaceemotion\LaravelEventSourcing\Tests;
+
+use function array_push;
+
+class AggregateRootTest extends \PHPUnit\Framework\TestCase
+{
+    /** @test */
+    public function it_records_events(): void
+    {
+        $root = TestAggregateRoot::new();
+
+        $root->set(['foo' => 'bar']);
+
+        $events = [];
+
+        array_push($events, ...$root->flushEvents());
+
+        self::assertCount(1, $events);
+    }
+
+    /** @test */
+    public function it_allows_shallow_copies(): void
+    {
+        $original = TestAggregateRoot::new();
+        $copy = TestAggregateRoot::forId($original->getId());
+
+        self::assertEquals($original->getId(), $copy->getId());
+    }
+}
